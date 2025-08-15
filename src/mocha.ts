7eb1c5ae5b559cc7c70b5337b0c75bc4003b19e3
degen0x <degen0x@gmail.com>
2025-08-15 15:47:38 +0400

    second commit

---
+++ src/mocha.ts
@@ -0,0 +1,71 @@
+import * as anchor from "@coral-xyz/anchor";
+import {Program, Wallet, web3} from "@coral-xyz/anchor";
+import { TokenSale } from "../target/types/token_sale";
+
+import {clusterApiUrl, Keypair, PublicKey} from "@solana/web3.js";
+import {BN} from "bn.js";
+import bs58 from "bs58"
+import {
+    getAssociatedTokenAddress,
+    getOrCreateAssociatedTokenAccount,
+    mintTo,
+} from "@solana/spl-token";
+
+describe("tokenSale", () => {
+    const connection = new web3.Connection(clusterApiUrl("devnet"));
+    const adminKeypair = web3.Keypair.fromSecretKey(Uint8Array.from([44,80,171,173,241,101,64,139,192,26,111,88,60,120,80,114,200,96,47,191,160,50,198,195,122,228,56,19,171,55,90,146,5,197,173,76,91,118,205,122,8,208,252,198,84,160,163,53,125,204,227,30,240,83,34,80,16,56,9,126,211,173,107,230]));
+    // enter your secret key
+    const userKeypair = web3.Keypair.fromSecretKey(bs58.decode(<enter your private key>));
+    const wallet = new Wallet(userKeypair);
+
+    const provider = new anchor.AnchorProvider(connection, wallet)
+    anchor.setProvider(provider);
+
+    const program = anchor.workspace.TokenSale as Program<TokenSale>;
+
+    const mint = new web3.PublicKey("5UdCc3A1b65nyjftajQ8kMUCQRH2kqChL7oBGwdY1hde");
+    const config = new web3.PublicKey("6nsRQQqZ4WHkmnPYv9vDDQuEzx6zfN93Q8kM9hSoQhUf")
+    const myUsdtMint = new web3.PublicKey("8YQ4Q8XAT6GBGRdQrPWASpjeJagppzhHzjj4wTsvHNK");
+    const myUsdcMint =  new web3.PublicKey("GUxS4tgDFF1hTtLPgJAsUa6vvEwcBYBJGcokPsPVZMqb");
+
+    it("Init sale", async () => {
+        // (_1st stage price_, _2nd stage price_, _stages border_, _2nd stage duration in sec_, _limit per wallet_)
+        const tx = await program.methods.initSale(new BN(200), new BN(1000), new BN(88888000000), new BN(1209600), new BN(2222000000))
+            .accounts({
+                mint: mint,
+                usdtMint: myUsdtMint,
+                usdcMint: myUsdcMint,
+            })
+            // .simulate()
+        .rpc();
+        console.log("Your transaction signature", tx);
+    });
+
+    it.only("Buy", async () => {
+
+        const userUsdtAta = await getOrCreateAssociatedTokenAccount(connection, userKeypair, myUsdtMint, provider.publicKey);
+        const userUsdcAta = await getOrCreateAssociatedTokenAccount(connection, userKeypair, myUsdcMint, provider.publicKey);
+
+        await mintTo(connection, userKeypair, myUsdtMint, userUsdtAta.address, adminKeypair, 100000000000)
+        await mintTo(connection, userKeypair, myUsdcMint, userUsdcAta.address, adminKeypair, 100000000000)
+
+        const configUsdtAta = await getAssociatedTokenAddress(myUsdtMint, config, true);
+        const configUsdcAta = await getAssociatedTokenAddress(myUsdcMint, config, true);
+        const configAta = await getAssociatedTokenAddress(mint, config, true);
+        const userAta = await getAssociatedTokenAddress(mint, userKeypair.publicKey);
+        const userState = web3.PublicKey.findProgramAddressSync([Buffer.from("user"), userKeypair.publicKey.toBuffer()], new web3.PublicKey("8485PZTPiULpLaFxxRtuQHEK76m7oqeYLM3Bm5h4S6y7"))[0];
+
+        // (_amount in tokens_)
+        const tx = await program.methods.buy(new BN(100000000))
+            .accounts({
+                payer: userKeypair.publicKey,
+                mint: mint,
+                userStableAta: userUsdcAta.address,
+                vaultStableAta: configUsdcAta,
+            })
+            // .simulate();
+        .rpc();
+
+        console.log("Your transaction signature", tx);
+    });
+});
---
+++ target/types/token_sale.ts
@@ -0,0 +1,902 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/token_sale.json`.
+ */
+export type TokenSale = {
+  "address": "8485PZTPiULpLaFxxRtuQHEK76m7oqeYLM3Bm5h4S6y7",
+  "metadata": {
+    "name": "tokenSale",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
+    {
+      "name": "buy",
+      "discriminator": [
+        102,
+        6,
+        61,
+        18,
+        1,
+        218,
+        235,
+        234
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "userState",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  117,
+                  115,
+                  101,
+                  114
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "payer"
+              }
+            ]
+          }
+        },
+        {
+          "name": "mint"
+        },
+        {
+          "name": "vault",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "account",
+                "path": "config"
+              },
+              {
+                "kind": "const",
+                "value": [
+                  6,
+                  221,
+                  246,
+                  225,
+                  215,
+                  101,
+                  161,
+                  147,
+                  217,
+                  203,
+                  225,
+                  70,
+                  206,
+                  235,
+                  121,
+                  172,
+                  28,
+                  180,
+                  133,
+                  237,
+                  95,
+                  91,
+                  55,
+                  145,
+                  58,
+                  140,
+                  245,
+                  133,
+                  126,
+                  255,
+                  0,
+                  169
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "mint"
+              }
+            ],
+            "program": {
+              "kind": "const",
+              "value": [
+                140,
+                151,
+                37,
+                143,
+                78,
+                36,
+                137,
+                241,
+                187,
+                61,
+                16,
+                41,
+                20,
+                142,
+                13,
+                131,
+                11,
+                90,
+                19,
+                153,
+                218,
+                255,
+                16,
+                132,
+                4,
+                142,
+                123,
+                216,
+                219,
+                233,
+                248,
+                89
+              ]
+            }
+          }
+        },
+        {
+          "name": "userTokenAta",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "account",
+                "path": "payer"
+              },
+              {
+                "kind": "const",
+                "value": [
+                  6,
+                  221,
+                  246,
+                  225,
+                  215,
+                  101,
+                  161,
+                  147,
+                  217,
+                  203,
+                  225,
+                  70,
+                  206,
+                  235,
+                  121,
+                  172,
+                  28,
+                  180,
+                  133,
+                  237,
+                  95,
+                  91,
+                  55,
+                  145,
+                  58,
+                  140,
+                  245,
+                  133,
+                  126,
+                  255,
+                  0,
+                  169
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "mint"
+              }
+            ],
+            "program": {
+              "kind": "const",
+              "value": [
+                140,
+                151,
+                37,
+                143,
+                78,
+                36,
+                137,
+                241,
+                187,
+                61,
+                16,
+                41,
+                20,
+                142,
+                13,
+                131,
+                11,
+                90,
+                19,
+                153,
+                218,
+                255,
+                16,
+                132,
+                4,
+                142,
+                123,
+                216,
+                219,
+                233,
+                248,
+                89
+              ]
+            }
+          }
+        },
+        {
+          "name": "userStableAta",
+          "writable": true
+        },
+        {
+          "name": "vaultStableAta",
+          "writable": true
+        },
+        {
+          "name": "config",
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  99,
+                  111,
+                  110,
+                  102,
+                  105,
+                  103
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        },
+        {
+          "name": "tokenProgram",
+          "address": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+        },
+        {
+          "name": "associatedTokenProgram",
+          "address": "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
+        }
+      ],
+      "args": [
+        {
+          "name": "stableAmount",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "close",
+      "discriminator": [
+        98,
+        165,
+        201,
+        177,
+        108,
+        65,
+        206,
+        96
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "mint",
+          "writable": true
+        },
+        {
+          "name": "vault",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "account",
+                "path": "config"
+              },
+              {
+                "kind": "const",
+                "value": [
+                  6,
+                  221,
+                  246,
+                  225,
+                  215,
+                  101,
+                  161,
+                  147,
+                  217,
+                  203,
+                  225,
+                  70,
+                  206,
+                  235,
+                  121,
+                  172,
+                  28,
+                  180,
+                  133,
+                  237,
+                  95,
+                  91,
+                  55,
+                  145,
+                  58,
+                  140,
+                  245,
+                  133,
+                  126,
+                  255,
+                  0,
+                  169
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "mint"
+              }
+            ],
+            "program": {
+              "kind": "const",
+              "value": [
+                140,
+                151,
+                37,
+                143,
+                78,
+                36,
+                137,
+                241,
+                187,
+                61,
+                16,
+                41,
+                20,
+                142,
+                13,
+                131,
+                11,
+                90,
+                19,
+                153,
+                218,
+                255,
+                16,
+                132,
+                4,
+                142,
+                123,
+                216,
+                219,
+                233,
+                248,
+                89
+              ]
+            }
+          }
+        },
+        {
+          "name": "config",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  99,
+                  111,
+                  110,
+                  102,
+                  105,
+                  103
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        },
+        {
+          "name": "tokenProgram",
+          "address": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+        },
+        {
+          "name": "associatedTokenProgram",
+          "address": "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "initSale",
+      "discriminator": [
+        41,
+        197,
+        251,
+        217,
+        167,
+        153,
+        95,
+        49
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "mint"
+        },
+        {
+          "name": "usdtMint"
+        },
+        {
+          "name": "usdcMint"
+        },
+        {
+          "name": "usdtVault",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "account",
+                "path": "config"
+              },
+              {
+                "kind": "const",
+                "value": [
+                  6,
+                  221,
+                  246,
+                  225,
+                  215,
+                  101,
+                  161,
+                  147,
+                  217,
+                  203,
+                  225,
+                  70,
+                  206,
+                  235,
+                  121,
+                  172,
+                  28,
+                  180,
+                  133,
+                  237,
+                  95,
+                  91,
+                  55,
+                  145,
+                  58,
+                  140,
+                  245,
+                  133,
+                  126,
+                  255,
+                  0,
+                  169
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "usdtMint"
+              }
+            ],
+            "program": {
+              "kind": "const",
+              "value": [
+                140,
+                151,
+                37,
+                143,
+                78,
+                36,
+                137,
+                241,
+                187,
+                61,
+                16,
+                41,
+                20,
+                142,
+                13,
+                131,
+                11,
+                90,
+                19,
+                153,
+                218,
+                255,
+                16,
+                132,
+                4,
+                142,
+                123,
+                216,
+                219,
+                233,
+                248,
+                89
+              ]
+            }
+          }
+        },
+        {
+          "name": "usdcVault",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "account",
+                "path": "config"
+              },
+              {
+                "kind": "const",
+                "value": [
+                  6,
+                  221,
+                  246,
+                  225,
+                  215,
+                  101,
+                  161,
+                  147,
+                  217,
+                  203,
+                  225,
+                  70,
+                  206,
+                  235,
+                  121,
+                  172,
+                  28,
+                  180,
+                  133,
+                  237,
+                  95,
+                  91,
+                  55,
+                  145,
+                  58,
+                  140,
+                  245,
+                  133,
+                  126,
+                  255,
+                  0,
+                  169
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "usdcMint"
+              }
+            ],
+            "program": {
+              "kind": "const",
+              "value": [
+                140,
+                151,
+                37,
+                143,
+                78,
+                36,
+                137,
+                241,
+                187,
+                61,
+                16,
+                41,
+                20,
+                142,
+                13,
+                131,
+                11,
+                90,
+                19,
+                153,
+                218,
+                255,
+                16,
+                132,
+                4,
+                142,
+                123,
+                216,
+                219,
+                233,
+                248,
+                89
+              ]
+            }
+          }
+        },
+        {
+          "name": "vault",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "account",
+                "path": "config"
+              },
+              {
+                "kind": "const",
+                "value": [
+                  6,
+                  221,
+                  246,
+                  225,
+                  215,
+                  101,
+                  161,
+                  147,
+                  217,
+                  203,
+                  225,
+                  70,
+                  206,
+                  235,
+                  121,
+                  172,
+                  28,
+                  180,
+                  133,
+                  237,
+                  95,
+                  91,
+                  55,
+                  145,
+                  58,
+                  140,
+                  245,
+                  133,
+                  126,
+                  255,
+                  0,
+                  169
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "mint"
+              }
+            ],
+            "program": {
+              "kind": "const",
+              "value": [
+                140,
+                151,
+                37,
+                143,
+                78,
+                36,
+                137,
+                241,
+                187,
+                61,
+                16,
+                41,
+                20,
+                142,
+                13,
+                131,
+                11,
+                90,
+                19,
+                153,
+                218,
+                255,
+                16,
+                132,
+                4,
+                142,
+                123,
+                216,
+                219,
+                233,
+                248,
+                89
+              ]
+            }
+          }
+        },
+        {
+          "name": "config",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  99,
+                  111,
+                  110,
+                  102,
+                  105,
+                  103
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        },
+        {
+          "name": "tokenProgram",
+          "address": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+        },
+        {
+          "name": "associatedTokenProgram",
+          "address": "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
+        }
+      ],
+      "args": [
+        {
+          "name": "price1",
+          "type": "u64"
+        },
+        {
+          "name": "price2",
+          "type": "u64"
+        },
+        {
+          "name": "priceBorder",
+          "type": "u64"
+        },
+        {
+          "name": "timer",
+          "type": "i64"
+        },
+        {
+          "name": "limitPerWallet",
+          "type": "u64"
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "config",
+      "discriminator": [
+        155,
+        12,
+        170,
+        224,
+        30,
+        250,
+        204,
+        130
+      ]
+    },
+    {
+      "name": "userState",
+      "discriminator": [
+        72,
+        177,
+        85,
+        249,
+        76,
+        167,
+        186,
+        126
+      ]
+    }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "amountIsTooLarge",
+      "msg": "The amount exceeds the remaining tokens"
+    },
+    {
+      "code": 6001,
+      "name": "saleEnded",
+      "msg": "Token sale is over"
+    },
+    {
+      "code": 6002,
+      "name": "limitPerWalletExceeded",
+      "msg": "User limit per wallet exceeded"
+    }
+  ],
+  "types": [
+    {
+      "name": "config",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "bump",
+            "type": "u8"
+          },
+          {
+            "name": "admin",
+            "type": "pubkey"
+          },
+          {
+            "name": "mint",
+            "type": "pubkey"
+          },
+          {
+            "name": "vault",
+            "type": "pubkey"
+          },
+          {
+            "name": "usdtVault",
+            "type": "pubkey"
+          },
+          {
+            "name": "usdcVault",
+            "type": "pubkey"
+          },
+          {
+            "name": "price1",
+            "type": "u64"
+          },
+          {
+            "name": "price2",
+            "type": "u64"
+          },
+          {
+            "name": "totalBuyed",
+            "type": "u64"
+          },
+          {
+            "name": "priceBorder",
+            "type": "u64"
+          },
+          {
+            "name": "secondStageDuration",
+            "type": "i64"
+          },
+          {
+            "name": "timer",
+            "type": "i64"
+          },
+          {
+            "name": "limitPerWallet",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "userState",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "totalBuyed",
+            "type": "u64"
+          }
+        ]
+      }
+    }
+  ]
+};
